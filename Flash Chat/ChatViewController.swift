d662dabbce2f1d7739c9a4fcc549c19fb450102c
LinaChasova <a.chasova@innopolis.ru>
2018-12-28 19:10:57 +0300

    display new images

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -34,6 +34,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         
         configureTableView()
+        retrieveMessages()
     }
 
     ///////////////////////////////////////////
@@ -45,13 +46,21 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell",
                                                  for: indexPath) as! CustomMessageCell
         
-        cell.messageBody.text = messageArray[indexPath.row]
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
+        
+        if messageArray[indexPath.row].sender == Auth.auth().currentUser?.email {
+            cell.messageBackground.backgroundColor = UIColor.blue
+        } else {
+            cell.messageBackground.backgroundColor = UIColor.green
+        }
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return messageArray.count
     }
     
     
@@ -120,6 +129,21 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func retrieveMessages() {
         let messageDB = Database.database().reference().child("Messages")
+        
+        messageDB.observe(.childAdded) { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String, String>
+            
+            let text = snapshotValue["messageBody"]!
+            let sender = snapshotValue["sender"]!
+            
+            let message = Message()
+            message.messageBody = text
+            message.sender = sender
+            
+            self.messageArray.append(message)
+            self.configureTableView()
+            self.messageTableView.reloadData()
+        }
     }
 
     
